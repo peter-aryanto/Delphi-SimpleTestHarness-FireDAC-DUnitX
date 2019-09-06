9b044cfa27edadfb6baaf73a4ac4f5e0f62bffc1
Peter Aryanto <peter.aryanto@gmail.com>
2019-09-06 22:04:47 +1000

    Added CommonDatabaseUpgraderRunner.

---
+++ CommonDatabaseUpgraderRunner.pas
@@ -0,0 +1,97 @@
+unit CommonDatabaseUpgraderRunner;
+
+interface
+
+type
+  TDatabaseUpgraderRunnerOnMessage = reference to procedure(const AMessage: string);
+
+  IPrivateCommonDatabaseUpgraderRunner = interface
+    ['{7EA27605-27C6-4D83-9B8B-010AC6B7FAE4}']
+    procedure SetOnMessage(const AOnMessage: TDatabaseUpgraderRunnerOnMessage);
+    {For property DatabaseLocation:}
+      function GetDatabaseLocation: string;
+      procedure SetDatabaseLocation(const ADatabaseLocation: string);
+    function GetErrorMessage: string;
+    function GetErrorDetails: string;
+  end;
+
+  ICommonDatabaseUpgraderRunner = interface(IPrivateCommonDatabaseUpgraderRunner)
+    ['{E158FA99-2A4D-4D7C-99BF-A19CAB0C67FA}']
+    property OnMessage: TDatabaseUpgraderRunnerOnMessage write SetOnMessage;
+    property DatabaseLocation: string read GetDatabaseLocation write SetDatabaseLocation;
+    function RunUpgrade: Boolean;
+    property ErrorMessage: string read GetErrorMessage;
+    property ErrorDetails: string read GetErrorDetails;
+  end;
+
+  TCommonDatabaseUpgraderRunner = class abstract(TInterfacedObject, ICommonDatabaseUpgraderRunner)
+  private
+    FOnMessage: TDatabaseUpgraderRunnerOnMessage;
+    FDatabaseLocation: string;
+    FErrorMessage: string;
+    FErrorDetails: string;
+  private {Interface methods}
+    procedure SetOnMessage(const AOnMessage: TDatabaseUpgraderRunnerOnMessage);
+    function GetErrorMessage: string;
+    function GetErrorDetails: string;
+  private
+    procedure ResetError;
+  protected {Interface methods}
+    {For property DatabaseLocation:}
+      function GetDatabaseLocation: string; virtual;
+      procedure SetDatabaseLocation(const ADatabaseLocation: string); virtual;
+  public {Interface methods}
+    function RunUpgrade: Boolean; virtual;
+  end;
+
+implementation
+
+uses
+  System.SysUtils;
+
+function TCommonDatabaseUpgraderRunner.RunUpgrade: Boolean;
+begin
+  ResetError;
+  Result := False;
+end;
+
+procedure TCommonDatabaseUpgraderRunner.SetOnMessage(
+  const AOnMessage: TDatabaseUpgraderRunnerOnMessage);
+begin
+  FOnMessage := AOnMessage;
+end;
+
+function TCommonDatabaseUpgraderRunner.GetDatabaseLocation;
+begin
+  Result := FDatabaseLocation;
+end;
+
+procedure TCommonDatabaseUpgraderRunner.SetDatabaseLocation(const ADatabaseLocation: string);
+var
+  LDatabaseLocation: string;
+begin
+  LDatabaseLocation := Trim(ADatabaseLocation);
+  if LDatabaseLocation = '' then
+    raise Exception.Create('Database location cannot be blank.');
+
+  FDatabaseLocation := ADatabaseLocation;
+  ResetError;
+end;
+
+function TCommonDatabaseUpgraderRunner.GetErrorMessage: string;
+begin
+  Result := FErrorMessage;
+end;
+
+function TCommonDatabaseUpgraderRunner.GetErrorDetails: string;
+begin
+  Result := FErrorDetails;
+end;
+
+procedure TCommonDatabaseUpgraderRunner.ResetError;
+begin
+  FErrorMessage := '';
+  FErrorDetails := '';
+end;
+
+end.
--- DatabaseUpgraderRunner.dpr
+++ DatabaseUpgraderRunner.dpr
@@ -6,11 +6,13 @@ program DatabaseUpgraderRunner;
 
 uses
   System.SysUtils,
-  BackupOriginalFile in 'Lib\BackupOriginalFile.pas';
+  BackupOriginalFile in 'Lib\BackupOriginalFile.pas',
+  CommonDatabaseUpgraderRunner in 'CommonDatabaseUpgraderRunner.pas';
 
 var
   GDbFileBackup: IBackupOriginalFile = nil;
   GIsSuccessful: Boolean = False;
+  Runner: ICommonDatabaseUpgraderRunner = nil;
 
 begin
 {$IFDEF DEBUG}
@@ -25,6 +27,11 @@ begin
     GDbFileBackup := TBackupOriginalFile.Create;
     GDbFileBackup.CreateBackup(ParamStr(1), FormatDateTime('yyyymmddhhnnss', Now));
 
+    Runner := TCommonDatabaseUpgraderRunner.Create;
+    Runner.OnMessage := procedure (const AMessage: string)
+      begin
+        Writeln(AMessage);
+      end;
     // Upgrade DB ...
 
     GIsSuccessful := True;
@@ -32,9 +39,18 @@ begin
   except
     on E: Exception do
     begin
-      GDbFileBackup.Rollback;
+      if Assigned(GDbFileBackup) then
+        GDbFileBackup.Rollback;
 
-      {Log.Error}Writeln(E.Message);
+      if Assigned(Runner) then
+      begin
+        if Runner.ErrorMessage <> '' then
+          {Log.Error}Writeln(Runner.ErrorMessage);
+        if Runner.ErrorDetails <> '' then
+          {Log.Debug}Writeln(Runner.ErrorDetails);
+      end
+      else
+        {Log.Error}Writeln(E.Message);
     end;
   end;
 
